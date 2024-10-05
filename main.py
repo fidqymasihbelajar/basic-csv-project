6ba3be93e46d2fb26f9913a9d3ba1d70db001f55
fidqymasihbelajar <fidqyalfarash@gmail.com>
2024-10-05 23:33:55 +0700

    adding function to convert inputted data into csv format

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+from datetime import datetime
+
 name_input = str(input("Enter your full name: "))
 idnumber_input = int(input("Enter your badge number: "))
 job_function_input = str(input("Enter your Job Function: "))
+
+record = {
+    'Name': name_input,
+    'Badge Number': idnumber_input,
+    'Job Function': job_function_input,
+    'Timestamp': datetime.now()
+}
+
+print(record)
